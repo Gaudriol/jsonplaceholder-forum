31625eba6ddfbc8f67bc793e7af4c69b34133b80
Gaudriol <romain.peter3@gmail.com>
2020-10-17 21:38:40 +0200

    feat: set up routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return <div>Coucou</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          Index with posts
+        </Route>
+        <Route exact path="/posts/:postId">
+          Post
+        </Route>
+        <Route exact path="/users/:userId">
+          User
+        </Route>
+        <Route path="*">Not found</Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
